3507cd7dce6edd3f62feb9ccdfeb93a885aee07f
BringMaster <icraftbg@gmail.com>
2023-08-19 13:59:49 +0300

    Update mysql-secure.sh

--- mysql-secure.sh
+++ mysql-secure.sh
@@ -8,6 +8,14 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password password ${P
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${PASS_MYSQL_ROOT}" # repeat password for the MySQL root user
 
 # Other Code.....
+mysql --user=root <<_EOF_
+  UPDATE mysql.user SET Password=PASSWORD('${PASS_MYSQL_ROOT}') WHERE User='root';
+  DELETE FROM mysql.user WHERE User='';
+  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+  DROP DATABASE IF EXISTS test;
+  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+  FLUSH PRIVILEGES;
+_EOF_
 mysql -uroot -p${PASS_MYSQL_ROOT} -e "CREATE DATABASE mc /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -uroot -p${PASS_MYSQL_ROOT} -e "GRANT ALL PRIVILEGES ON mc.* TO root@localhost IDENTIFIED BY '${PASS_MYSQL_ROOT}'"
 mysql -uroot -p${PASS_MYSQL_ROOT} -e "FLUSH PRIVILEGES;"
